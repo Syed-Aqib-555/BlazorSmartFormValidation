03b562db36c708afbf9bc75be148535978bf2b98
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-26 01:18:15 +0500

    Create UserModel with DataAnnotations validation

---
+++ BlazorSmartFormValidation/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSmartFormValidation.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "First Name is Required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; } = string.Empty;
+    }
+    
+}
